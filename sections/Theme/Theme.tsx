b5cc27cb6feb46341a9418540a2466fe3d544c61
AbraaoMorais <moraisabraao@gmail.com>
2023-08-18 22:28:51 -0300

    add files

--- sections/Theme/Theme.tsx
+++ sections/Theme/Theme.tsx
@@ -10,6 +10,7 @@ import { Head } from "$fresh/runtime.ts";
 import googleFontsLoader, {
   Props as GoogleFontsLoaderProps,
 } from "deco-sites/std/packs/font/loaders/googleFonts.ts";
+import { asset } from "$fresh/runtime.ts";
 
 export interface MainColors {
   /**
@@ -407,6 +408,109 @@ function Section({
     <Head>
       <meta name="theme-color" content={theme["primary"]} />
       <meta name="msapplication-TileColor" content={theme["primary"]} />
+
+      {/* default fonts */}
+      <style
+        type="text/css"
+        dangerouslySetInnerHTML={{
+          __html: `
+          /* latin-ext */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 400;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay400ext.woff2")
+          }) format('woff2');
+              unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+            }
+          
+            /* latin */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 400;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay400.woff2")
+          }) format('woff2');
+              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            }
+          
+            /* latin-ext */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 500;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay500.woff2")
+          }) format('woff2');
+              unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+            }
+          
+            /* latin */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 500;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay500.woff2")
+          }) format('woff2');
+              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            }
+          
+            /* latin-ext */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 600;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay600ext.woff2")
+          }) format('woff2');
+              unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+            }
+          
+            /* latin */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 600;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay600.woff2")
+          }) format('woff2');
+              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            }
+          
+            /* latin-ext */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 700;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay700ext.woff2")
+          }) format('woff2');
+              unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+            }
+            /* latin */
+            @font-face {
+              font-family: 'Red Hat Display';
+              font-style: normal;
+              font-weight: 700;
+              font-display: swap;
+              src: url(${
+            asset("fonts/redhatdisplay700.woff2")
+          }) format('woff2');
+              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            }`,
+        }}
+      />
+
       {fontsSheet?.map((fontSheet) => (
         <style
           type="text/css"
